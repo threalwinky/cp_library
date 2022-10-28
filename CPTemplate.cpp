9e998d430327a5d479761314ae65b94581e043db
Vũ <voquangvu09112006@gmail.com>
2022-10-28 23:30:09 +0700

    upload files

--- CPTemplate.cpp
+++ CPTemplate.cpp
@@ -1,28 +1,60 @@
+/**
+ *   author: winky
+ *   created: 2022-10-28 23:29:27
+**/
+
+// Obey my waifu
+//⢸⣿⣿⣿⣿⠃⠄⢀⣴⡾⠃⠄⠄⠄⠄⠄⠈⠺⠟⠛⠛⠛⠛⠻⢿⣿⣿⣿⣿⣶⣤⡀⠄
+//⢸⣿⣿⣿⡟⢀⣴⣿⡿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣸⣿⣿⣿⣿⣿⣿⣿⣷
+//⢸⣿⣿⠟⣴⣿⡿⡟⡼⢹⣷⢲⡶⣖⣾⣶⢄⠄⠄⠄⠄⠄⢀⣼⣿⢿⣿⣿⣿⣿⣿⣿⣿
+//⢸⣿⢫⣾⣿⡟⣾⡸⢠⡿⢳⡿⠍⣼⣿⢏⣿⣷⢄⡀⠄⢠⣾⢻⣿⣸⣿⣿⣿⣿⣿⣿⣿
+//⡿⣡⣿⣿⡟⡼⡁⠁⣰⠂⡾⠉⢨⣿⠃⣿⡿⠍⣾⣟⢤⣿⢇⣿⢇⣿⣿⢿⣿⣿⣿⣿⣿
+//⣱⣿⣿⡟⡐⣰⣧⡷⣿⣴⣧⣤⣼⣯⢸⡿⠁⣰⠟⢀⣼⠏⣲⠏⢸⣿⡟⣿⣿⣿⣿⣿⣿
+//⣿⣿⡟⠁⠄⠟⣁⠄⢡⣿⣿⣿⣿⣿⣿⣦⣼⢟⢀⡼⠃⡹⠃⡀⢸⡿⢸⣿⣿⣿⣿⣿⡟
+//⣿⣿⠃⠄⢀⣾⠋⠓⢰⣿⣿⣿⣿⣿⣿⠿⣿⣿⣾⣅⢔⣕⡇⡇⡼⢁⣿⣿⣿⣿⣿⣿⢣
+//⣿⡟⠄⠄⣾⣇⠷⣢⣿⣿⣿⣿⣿⣿⣿⣭⣀⡈⠙⢿⣿⣿⡇⡧⢁⣾⣿⣿⣿⣿⣿⢏⣾
+//⣿⡇⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢻⠇⠄⠄⢿⣿⡇⢡⣾⣿⣿⣿⣿⣿⣏⣼⣿
+//⣿⣷⢰⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢰⣧⣀⡄⢀⠘⡿⣰⣿⣿⣿⣿⣿⣿⠟⣼⣿⣿
+//⢹⣿⢸⣿⣿⠟⠻⢿⣿⣿⣿⣿⣿⣿⣿⣶⣭⣉⣤⣿⢈⣼⣿⣿⣿⣿⣿⣿⠏⣾⣹⣿⣿
+//⢸⠇⡜⣿⡟⠄⠄⠄⠈⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟⣱⣻⣿⣿⣿⣿⣿⠟⠁⢳⠃⣿⣿⣿
+//⠄⣰⡗⠹⣿⣄⠄⠄⠄⢀⣿⣿⣿⣿⣿⣿⠟⣅⣥⣿⣿⣿⣿⠿⠋⠄⠄⣾⡌⢠⣿⡿⠃
+//⠜⠋⢠⣷⢻⣿⣿⣶⣾⣿⣿⣿⣿⠿⣛⣥⣾⣿⠿⠟⠛⠉⠄⠄⠄⠄⣾⡌⢠⣿⡿⠃
+
 #include                    <bits/stdc++.h>
 using namespace             std;
-//#pragma comment(linker, "/stack:200000000")
-//#pragma GCC optimize("Ofast,unroll-loops,tree-vectorize")
-//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+//#pragma GCC               optimize("Ofast,unroll-loops,inline")
+//#pragma GCC               target("bmi,bmi2,lzcnt,popcnt")
+//#pragma GCC               target("movbe")
+//#pragma GCC               target("aes,pclmul,rdrnd")
+//#pragma GCC               target("avx,avx2,f16c,fma,sse,sse2,sse3,ssse3,sse4,sse4.1,sse4.2,abm,mmx,avx,tune=native")
 #define FASTIO              ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define FILEIO1(__, ___)    freopen(__, "r", stdin);freopen(___, "w", stdout)
-#define FILEIO2(__)         freopen(__, "r", stdin);
-#define FILEIO3             freopen("sol.inp", "r", stdin);freopen("sol.out", "w", stdout)
-#define fu(__, ___, ____)   for (int __ = ___; __<=____; __++)
-#define fd(__, ___, ____)   for (int __ = ___; __>=____; __--)
-#define fa(__, ___)         for (auto __ : ___)
+#define FILEIO1(a)          freopen(a + "inp", "r", stdin);freopen(a + "out", "w", stdout)
+#define FILEIO2(a)          freopen(a + "inp", "r", stdin);
+#define FILEIO3             freopen("a.inp", "r", stdin);freopen("a.out", "w", stdout)
+#define fu(i, a, b)         for (int i = a; i<=b; i++)
+#define fd(i, a, b)         for (int i = a; i>=b; i--)
+#define fa(it, x)           for (auto it : x)
+#define tri(a, b, c)        ((a) ? (b) : (c))
+#define carr(arr, n)        cout<<"(";fu(i, 1, n ){cout<<arr[i]<<tri(i == n, "", ", ");}cout<<")\n";
 #define fi                  first
 #define se                  second
-#define all(__)             __.fi << " " << __.se <<"\n"
-#define debug(x)            cout<<x<<" ";
+#define all(it)             it.fi << " " << it.se <<"\n"
+#define debug(x)            cerr<<x<<" ";
+#define comma(x)            fixed << setprecision((x))
+//define int                long long
 typedef pair<int, int>      ii;
-typedef int64_t             ll;
+typedef long long           ll;
 typedef vector<int>         vi;
 typedef vector<ii>          vii;
 typedef vector<ll>          vll;
-const int MAX =             1e5;
+const int MAX =             3e5;
 const int MOD =             1e9 + 7;
 const int inf =             2147483647;
 const double PI =           3.14159265358979;
 /*----- ----- ----- ----- ----- ----- ----- ----- ----- -----*/
 
-int main(){$1}
+signed main(){
+    //FILEIO3;
+    //FASTIO;
+    
+}
