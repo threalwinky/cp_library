1c02137b0f9697fba3960ae0e538bea68a26b7cc
Vũ <105662081+winkyyyyy@users.noreply.github.com>
2023-03-30 23:33:37 +0700

    Add files via upload

---
+++ Codeforces/1374A.cpp
@@ -0,0 +1,67 @@
+/**
+ *   author: winky
+**/
+
+// Obey my waifu
+//⢸⣿⣿⣿⣿⠃⠄⢀⣴⡾⠃⠄⠄⠄⠄⠄⠈⠺⠟⠛⠛⠛⠛⠻⢿⣿⣿⣿⣿⣶⣤⡀⠄
+//⢸⣿⣿⣿⡟⢀⣴⣿⡿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣸⣿⣿⣿⣿⣿⣿⣿⣷
+//⢸⣿⣿⠟⣴⣿⡿⡟⡼⢹⣷⢲⡶⣖⣾⣶⢄⠄⠄⠄⠄⠄⢀⣼⣿⢿⣿⣿⣿⣿⣿⣿⣿
+//⢸⣿⢫⣾⣿⡟⣾⡸⢠⡿⢳⡿⠍⣼⣿⢏⣿⣷⢄⡀⠄⢠⣾⢻⣿⣸⣿⣿⣿⣿⣿⣿⣿
+//⡿⣡⣿⣿⡟⡼⡁⠁⣰⠂⡾⠉⢨⣿⠃⣿⡿⠍⣾⣟⢤⣿⢇⣿⢇⣿⣿⢿⣿⣿⣿⣿⣿
+//⣱⣿⣿⡟⡐⣰⣧⡷⣿⣴⣧⣤⣼⣯⢸⡿⠁⣰⠟⢀⣼⠏⣲⠏⢸⣿⡟⣿⣿⣿⣿⣿⣿
+//⣿⣿⡟⠁⠄⠟⣁⠄⢡⣿⣿⣿⣿⣿⣿⣦⣼⢟⢀⡼⠃⡹⠃⡀⢸⡿⢸⣿⣿⣿⣿⣿⡟
+//⣿⣿⠃⠄⢀⣾⠋⠓⢰⣿⣿⣿⣿⣿⣿⠿⣿⣿⣾⣅⢔⣕⡇⡇⡼⢁⣿⣿⣿⣿⣿⣿⢣
+//⣿⡟⠄⠄⣾⣇⠷⣢⣿⣿⣿⣿⣿⣿⣿⣭⣀⡈⠙⢿⣿⣿⡇⡧⢁⣾⣿⣿⣿⣿⣿⢏⣾
+//⣿⡇⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢻⠇⠄⠄⢿⣿⡇⢡⣾⣿⣿⣿⣿⣿⣏⣼⣿
+//⣿⣷⢰⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢰⣧⣀⡄⢀⠘⡿⣰⣿⣿⣿⣿⣿⣿⠟⣼⣿⣿
+//⢹⣿⢸⣿⣿⠟⠻⢿⣿⣿⣿⣿⣿⣿⣿⣶⣭⣉⣤⣿⢈⣼⣿⣿⣿⣿⣿⣿⠏⣾⣹⣿⣿
+//⢸⠇⡜⣿⡟⠄⠄⠄⠈⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟⣱⣻⣿⣿⣿⣿⣿⠟⠁⢳⠃⣿⣿⣿
+//⠄⣰⡗⠹⣿⣄⠄⠄⠄⢀⣿⣿⣿⣿⣿⣿⠟⣅⣥⣿⣿⣿⣿⠿⠋⠄⠄⣾⡌⢠⣿⡿⠃
+//⠜⠋⢠⣷⢻⣿⣿⣶⣾⣿⣿⣿⣿⠿⣛⣥⣾⣿⠿⠟⠛⠉⠄⠄⠄⠄⣾⡌⢠⣿⡿⠃
+
+#include                    <bits/stdc++.h>
+using namespace             std;
+//#pragma GCC               optimize("Ofast,unroll-loops,inline")
+//#pragma GCC               target("bmi,bmi2,lzcnt,popcnt")
+//#pragma GCC               target("movbe")
+//#pragma GCC               target("aes,pclmul,rdrnd")
+//#pragma GCC               target("avx,avx2,f16c,fma,sse,sse2,sse3,ssse3,sse4,sse4.1,sse4.2,abm,mmx,avx,tune=native")
+#define FASTIO              ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define FILEIO1(a)          freopen(a + "inp", "r", stdin);freopen(a + "out", "w", stdout)
+#define FILEIO2(a)          freopen(a + "inp", "r", stdin);
+#define FILEIO3             freopen("a.inp", "r", stdin);freopen("a.out", "w", stdout)
+#define fu(i, a, b)         for (int i = a; i<=b; i++)
+#define fd(i, a, b)         for (int i = a; i>=b; i--)
+#define fa(it, x)           for (auto it : x)
+#define tri(a, b, c)        ((a) ? (b) : (c))
+#define carr(arr, n)        cout<<"(";fu(i, 1, n ){cout<<arr[i]<<tri(i == n, "", ", ");}cout<<")\n";
+#define fi                  first
+#define se                  second
+#define all(it)             it.fi << " " << it.se <<"\n"
+#define debug(x)            cerr<<x<<" ";
+#define comma(x)            fixed << setprecision((x))
+//define int                long long
+typedef pair<int, int>      ii;
+typedef long long           ll;
+typedef vector<int>         vi;
+typedef vector<ii>          vii;
+typedef vector<ll>          vll;
+const int MAX =             3e5;
+const int MOD =             1e9 + 7;
+const int inf =             2147483647;
+const double PI =           3.14159265358979;
+/*----- ----- ----- ----- ----- ----- ----- ----- ----- -----*/
+
+signed main(){
+    //FILEIO3;
+    FASTIO;
+    int T;
+    cin>>T;
+    while (T--){
+    	int x, y, n;
+    	cin>>x>>y>>n;
+    	int a = (n - y)/x;
+    	cout<<a * x + y;
+    	cout<<"\n";
+    }
+}
---
+++ Codeforces/749A.cpp
@@ -0,0 +1,87 @@
+/**
+ *   author: winky
+**/
+
+// Obey my waifu
+//⢸⣿⣿⣿⣿⠃⠄⢀⣴⡾⠃⠄⠄⠄⠄⠄⠈⠺⠟⠛⠛⠛⠛⠻⢿⣿⣿⣿⣿⣶⣤⡀⠄
+//⢸⣿⣿⣿⡟⢀⣴⣿⡿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣸⣿⣿⣿⣿⣿⣿⣿⣷
+//⢸⣿⣿⠟⣴⣿⡿⡟⡼⢹⣷⢲⡶⣖⣾⣶⢄⠄⠄⠄⠄⠄⢀⣼⣿⢿⣿⣿⣿⣿⣿⣿⣿
+//⢸⣿⢫⣾⣿⡟⣾⡸⢠⡿⢳⡿⠍⣼⣿⢏⣿⣷⢄⡀⠄⢠⣾⢻⣿⣸⣿⣿⣿⣿⣿⣿⣿
+//⡿⣡⣿⣿⡟⡼⡁⠁⣰⠂⡾⠉⢨⣿⠃⣿⡿⠍⣾⣟⢤⣿⢇⣿⢇⣿⣿⢿⣿⣿⣿⣿⣿
+//⣱⣿⣿⡟⡐⣰⣧⡷⣿⣴⣧⣤⣼⣯⢸⡿⠁⣰⠟⢀⣼⠏⣲⠏⢸⣿⡟⣿⣿⣿⣿⣿⣿
+//⣿⣿⡟⠁⠄⠟⣁⠄⢡⣿⣿⣿⣿⣿⣿⣦⣼⢟⢀⡼⠃⡹⠃⡀⢸⡿⢸⣿⣿⣿⣿⣿⡟
+//⣿⣿⠃⠄⢀⣾⠋⠓⢰⣿⣿⣿⣿⣿⣿⠿⣿⣿⣾⣅⢔⣕⡇⡇⡼⢁⣿⣿⣿⣿⣿⣿⢣
+//⣿⡟⠄⠄⣾⣇⠷⣢⣿⣿⣿⣿⣿⣿⣿⣭⣀⡈⠙⢿⣿⣿⡇⡧⢁⣾⣿⣿⣿⣿⣿⢏⣾
+//⣿⡇⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢻⠇⠄⠄⢿⣿⡇⢡⣾⣿⣿⣿⣿⣿⣏⣼⣿
+//⣿⣷⢰⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢰⣧⣀⡄⢀⠘⡿⣰⣿⣿⣿⣿⣿⣿⠟⣼⣿⣿
+//⢹⣿⢸⣿⣿⠟⠻⢿⣿⣿⣿⣿⣿⣿⣿⣶⣭⣉⣤⣿⢈⣼⣿⣿⣿⣿⣿⣿⠏⣾⣹⣿⣿
+//⢸⠇⡜⣿⡟⠄⠄⠄⠈⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟⣱⣻⣿⣿⣿⣿⣿⠟⠁⢳⠃⣿⣿⣿
+//⠄⣰⡗⠹⣿⣄⠄⠄⠄⢀⣿⣿⣿⣿⣿⣿⠟⣅⣥⣿⣿⣿⣿⠿⠋⠄⠄⣾⡌⢠⣿⡿⠃
+//⠜⠋⢠⣷⢻⣿⣿⣶⣾⣿⣿⣿⣿⠿⣛⣥⣾⣿⠿⠟⠛⠉⠄⠄⠄⠄⣾⡌⢠⣿⡿⠃
+
+#include                    <bits/stdc++.h>
+using namespace             std;
+//#pragma GCC               optimize("Ofast,unroll-loops,inline")
+//#pragma GCC               target("bmi,bmi2,lzcnt,popcnt")
+//#pragma GCC               target("movbe")
+//#pragma GCC               target("aes,pclmul,rdrnd")
+//#pragma GCC               target("avx,avx2,f16c,fma,sse,sse2,sse3,ssse3,sse4,sse4.1,sse4.2,abm,mmx,avx,tune=native")
+#define FASTIO              ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define FILEIO1(a)          freopen(a + "inp", "r", stdin);freopen(a + "out", "w", stdout)
+#define FILEIO2(a)          freopen(a + "inp", "r", stdin);
+#define FILEIO3             freopen("a.inp", "r", stdin);freopen("a.out", "w", stdout)
+#define fu(i, a, b)         for (int i = a; i<=b; i++)
+#define fd(i, a, b)         for (int i = a; i>=b; i--)
+#define fa(it, x)           for (auto it : x)
+#define tri(a, b, c)        ((a) ? (b) : (c))
+#define carr(arr, n)        cout<<"(";fu(i, 1, n ){cout<<arr[i]<<tri(i == n, "", ", ");}cout<<")\n";
+#define fi                  first
+#define se                  second
+#define all(it)             it.fi << " " << it.se <<"\n"
+#define debug(x)            cerr<<x<<" ";
+#define comma(x)            fixed << setprecision((x))
+//define int                long long
+typedef pair<int, int>      ii;
+typedef long long           ll;
+typedef vector<int>         vi;
+typedef vector<ii>          vii;
+typedef vector<ll>          vll;
+const int MAX =             3e5;
+const int MOD =             1e9 + 7;
+const int inf =             2147483647;
+const double PI =           3.14159265358979;
+/*----- ----- ----- ----- ----- ----- ----- ----- ----- -----*/
+
+bool F[MAX];
+
+void era(){
+	F[0] = F[1] = 0;
+	for (int i = 2; i<200000; i++){
+		if (F[i] == 0){
+			for (int j = 2*i; j < 200000; j+=i)
+			{
+				F[j] = 1;
+			}
+		}
+	}
+}
+
+signed main(){
+    //FILEIO3;
+    FASTIO;
+    int x;
+    cin>>x;
+    if (x % 2 == 0){
+    	cout<<x / 2<<"\n";
+    	for (int i = 0; i<x/2; i++){
+    		cout<<2<<" ";
+    	}
+    }
+    else{
+    	cout<<x / 2<<"\n";
+    	for (int i = 0; i<x/2 - 1; i++){
+    		cout<<2<<" ";
+    	}
+    	cout<<3;
+    }
+}
