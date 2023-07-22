2235bc607739d1ecbecf8e79e8b65a480dfbea7c
Vũ <105662081+threalwinky@users.noreply.github.com>
2023-07-22 23:26:30 +0700

    Update CPTemplate.cpp

--- CPTemplate.cpp
+++ CPTemplate.cpp
@@ -1,20 +1,20 @@
 #include				<bits/stdc++.h>
-using 					namespace std;
-#define fi 				first
-#define se 				second
-#define pb(x) 			push_back(x)
-#define endl 			cout<<"\n";
-//#define int 			long long
-typedef 				vector<int> vi;
+using					namespace std;
+#define fi				first
+#define se				second
+#define pb(x)				push_back(x)
+#define endl				cout<<"\n";
+//#define int				long long
+typedef					vector<int> vi;
 typedef					pair<int, int> ii;
-const string F =		"sample";
-const string IF = 		F + ".inp";
-const string OF =		F + ".out";
-const int maxN = 		1e6;
-const int mod  = 		1e9 + 7;
-void opf(bool c){		if (c == 1){	
-freopen(IF.c_str(), 	"r", stdin);
-freopen(OF.c_str(), 	"w", stdout);}}
+const string F =			"sample";
+const string IF =			F + ".inp";
+const string OF =			F + ".out";
+const int maxN =			1e6;
+const int mod  =			1e9 + 7;
+void opf(bool c){			if (c == 1){	
+freopen(IF.c_str(),			"r", stdin);
+freopen(OF.c_str(),			"w", stdout);}}
 // bool mem1;
 // void clockt(){
 // 	bool mem2;
