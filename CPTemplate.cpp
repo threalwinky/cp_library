76aa6662477bc1ef6e7adffbded6eea2b17dc995
Vũ <105662081+threalwinky@users.noreply.github.com>
2023-07-23 00:03:03 +0700

    Update CPTemplate.cpp

--- CPTemplate.cpp
+++ CPTemplate.cpp
@@ -1,20 +1,20 @@
-#include				<bits/stdc++.h>
-using					namespace std;
-#define fi				first
-#define se				second
-#define pb(x)				push_back(x)
-#define endl				cout<<"\n";
-//#define int				long long
-typedef					vector<int> vi;
-typedef					pair<int, int> ii;
-const string F =			"sample";
-const string IF =			F + ".inp";
-const string OF =			F + ".out";
-const int maxN =			1e6;
-const int mod  =			1e9 + 7;
-void opf(bool c){			if (c == 1){	
-freopen(IF.c_str(),			"r", stdin);
-freopen(OF.c_str(),			"w", stdout);}}
+#include                    <bits/stdc++.h>
+using                       namespace std;
+#define fi                  first
+#define se                  second
+#define pb(x)               push_back(x)
+#define endl                cout<<"\n";
+//#define int               long long
+typedef pair<int, int>      ii;
+typedef vector<int>         vi;
+const string F =            "sample";
+const string IF =           F + ".inp";
+const string OF =           F + ".out";
+const int maxN =            1e6;
+const int mod  =            1e9 + 7;
+void opf(bool c){           if (c == 1){	
+freopen(IF.c_str(),         "r", stdin);
+freopen(OF.c_str(),         "w", stdout);}}
 // bool mem1;
 // void clockt(){
 // 	bool mem2;
@@ -28,6 +28,6 @@ freopen(OF.c_str(),			"w", stdout);}}
 signed main(){
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
-	opf(1);
-	
+	opf(0);
+	${1}
 }
